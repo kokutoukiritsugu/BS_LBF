0fbce573384f71590fb139609048e7129b839de8
kokutoukiritsugu <kokutoukiritsugu@gmail.com>
2017-05-11 13:07:53 +0800

    first version.

--- src/lbftofile.java
+++ src/lbftofile.java
@@ -5,49 +5,44 @@ import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.charset.Charset;
 
-/**
- * Created by kiritsugu on 2017-5-10.
- */
-public class Main {
+public class lbftofile {
     public static void main(String[] args) throws IOException {
         String lbfFileName = args.length > 0 ? args[0] : "Localizedjpn.lbf";
         String lbfFolderName = FilenameUtils.getBaseName(lbfFileName);
+        File f = new File(lbfFolderName);
+        f.mkdirs();
 
         FileInputStream fis = new FileInputStream(lbfFileName);
         BufferedInputStream bis = new BufferedInputStream(fis);
-
         while (true) {
+            // 1 bit - filename length
             byte[] fileNameSizeByte = new byte[1];
-            if (bis.read(fileNameSizeByte) == -1) {
-                break;
-            }
+            if (bis.read(fileNameSizeByte) == -1) break;
+            // skip c style string end \0 ch
             int fileNameSize = (fileNameSizeByte[0] - 1) * 2;
-            System.out.println(fileNameSize);
 
+            // filename
             byte[] fileNameByte = new byte[fileNameSize];
             bis.read(fileNameByte);
             String fileName = new String(fileNameByte, Charset.forName("utf-16le"));
-            System.out.println(fileName);
 
+            // skip c style string end \0 ch
             bis.skip(2);
 
+            // 4 bit - file length
             byte[] fileContentSizeByte = new byte[4];
             bis.read(fileContentSizeByte);
-            ByteBuffer bb = ByteBuffer.wrap(fileContentSizeByte);
-            bb.order(ByteOrder.LITTLE_ENDIAN);
-            int fileContentSize = bb.getInt();
-            System.out.println(fileContentSize);
+            int fileContentSize = ByteBuffer.wrap(fileContentSizeByte).order(ByteOrder.LITTLE_ENDIAN).getInt();
 
+            // file
             byte[] fileContentByte = new byte[fileContentSize];
             bis.read(fileContentByte);
-            String s = new String(fileContentByte, Charset.forName("utf-16le"));
-            System.out.println(s);
 
             FileOutputStream fos = new FileOutputStream(lbfFolderName + "\\" + fileName);
             BufferedOutputStream bos = new BufferedOutputStream(fos);
             bos.write(fileContentByte);
             bos.close();
-
         }
+        bis.close();
     }
 }
