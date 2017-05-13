ed4187dc37c87aa85fa7a4b225d005c03e8839b7
kokutoukiritsugu <kokutoukiritsugu@gmail.com>
2017-05-13 16:37:43 +0800

    something

--- src/bs2_lbftofile.java
+++ src/bs2_lbftofile.java
@@ -18,21 +18,45 @@ public class bs2_lbftofile {
             // 1 bit - filename length
             byte[] fileNameSizeByte = new byte[1];
             if (bis.read(fileNameSizeByte) == -1) break;
+            System.out.println(fileNameSizeByte[0]);
             // skip c style string end \0 ch
             int fileNameSize = (fileNameSizeByte[0] - 1) * 2;
+            System.out.println(fileNameSize);
 
             // filename
             byte[] fileNameByte = new byte[fileNameSize];
             bis.read(fileNameByte);
             String fileName = new String(fileNameByte, Charset.forName("utf-16le"));
+            System.out.println(fileName);
 
             // skip c style string end \0 ch
             bis.skip(2);
 
             // 1 bit - file length
-            byte[] fileContentSizeByte = new byte[1];
-            bis.read(fileContentSizeByte);
-            int fileContentSize = fileContentSizeByte[0] * 2;
+            byte[] fileContentSizeByte1 = new byte[1];
+            bis.read(fileContentSizeByte1);
+            int fileContentSizeByte1_int = ((int) fileContentSizeByte1[0]) & 0xff;
+
+            int out = fileContentSizeByte1_int;
+            if (fileContentSizeByte1_int > 0x3f) {
+
+                byte[] fileContentSizeByte2 = new byte[1];
+                bis.read(fileContentSizeByte2);
+                int fileContentSizeByte2_int = ((int) fileContentSizeByte2[0]) & 0xff;
+
+                out += (fileContentSizeByte2_int - 1) * 64;
+                if (fileContentSizeByte2_int > 0x7f) {
+
+                    byte[] fileContentSizeByte3 = new byte[1];
+                    bis.read(fileContentSizeByte3);
+                    int fileContentSizeByte3_int = ((int) fileContentSizeByte3[0]) & 0xff;
+
+                    out += (fileContentSizeByte3_int - 1) * 8192;
+
+                }
+            }
+            int fileContentSize = out * 2;
+            System.out.println("fileContentSize " + fileContentSize * 2);
 
             // file
             byte[] fileContentByte = new byte[fileContentSize];
